70c98fc6f6b3d20826a4de21c70579bcc53f0751
Luis Manuel <luisma.suarez@hotmail.com>
2017-08-21 00:49:39 -0500

    Added ranking and colors -Adds dynamic classes via string interpolation -Created scss variables and used darken css function

--- server/main.js
+++ server/main.js
@@ -1,6 +1,20 @@
 import {Meteor} from "meteor/meteor";
+// Players is being used by meteor to setup DDP, it's supposed to be here.
 import {Players} from "./../imports/api/players";
 Meteor.startup(() => {
 
 });
 
+// // object spread operator
+// let house = {
+//     bedrooms: 2,
+//     bathrooms: 1
+// };
+// let yearBuilt = 1992;
+//
+// console.log({
+//     ...house,
+//     bedrooms:4,
+//     yearBuilt,
+//     flooring: 'carpet'
+// });
